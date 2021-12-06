85499789b32510a52589de0f7c125fa4a33d3ade
AidenTran15 <aidenkiettran@gmail.com>
2021-12-06 16:23:16 +1100

    Set up video capture

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import cv2
 import time
 import datetime
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    cv2.imshow("Camera", frame)
+    if cv2.waitkey(1) == ord('q'):
+        break
