54ab9d94a187fbc6534d088623f971802de659b0
AidenTran15 <aidenkiettran@gmail.com>
2021-12-08 22:19:24 +1100

    Added face and body cascade

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import datetime
 
 cap = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
 while True:
     _, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) == ord('q'):
         break
