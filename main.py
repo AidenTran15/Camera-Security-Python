08863abed0df1e02389b03b9b28cb97f1a467480
AidenTran15 <aidenkiettran@gmail.com>
2021-12-08 17:57:21 +1100

    Display webcam video

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
     cv2.imshow("Camera", frame)
-    if cv2.waitkey(1) == ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
+    
+cap.release()
+cv2.destroyAllWindows()
